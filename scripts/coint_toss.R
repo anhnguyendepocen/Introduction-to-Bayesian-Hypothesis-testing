80b7022ebb67b35970765824e96fa3f583aa491d
Dr Peter Humburg <peter.humburg@mq.edu.au>
2018-09-25 13:27:33 +1000

    Add large dataset

--- scripts/coint_toss.R
+++ scripts/coint_toss.R
@@ -5,5 +5,8 @@ plot(1:1000, heads, type='l', xlab="Number of flips", ylab="Proportion of Heads"
 abline(h=0.5, lty=2, lwd=1.5)
 
 
-sum(smpl[1:100])
+set.seed(20180921)
+smpl <- rbinom(1500, 1, 0.55)
+sum(smpl)
 write.csv(cbind(toss=1:100, heads=smpl[1:100]), file="data/coin_toss.csv", row.names = FALSE)
+write.csv(cbind(toss=1:1500, heads=smpl), file="data/coin_toss_large.csv", row.names = FALSE)
